920645a7ac88a1085aa9dccdba22875e11137591
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2026-03-12 23:37:14 -0300

    refactor(logger): remove globais e reimplementa ativacao com handlers nativos
    - Remove FORMAT e logging.basicConfig() globais que vazavam configuracoes para outras bibliotecas
    - Remove atributo de classe DISABLED; controle de estado delegado ao modulo logging
    - Construtor limpa handlers herdados e adiciona NullHandler por padrao (mensagens descartadas silenciosamente)
    - activate() aceita level, console, file_log e filename; gera nome de arquivo com timestamp se omitido
    - Metodos warn/error/log/debug agora delegam diretamente ao self.logger sem condicional DISABLED

--- quantumnet/utils/logger.py
+++ quantumnet/utils/logger.py
@@ -1,41 +1,55 @@
 import logging
+from datetime import datetime
 from ..exceptions import SingletonError
 
-FORMAT = '%(asctime)s: %(message)s'
-logging.basicConfig(format=FORMAT)
 
 class Logger(object):
     __instance = None
-    DISABLED = True
 
     def __init__(self):
-            if Logger.__instance is None:
-                self.logger = logging.getLogger('QuantumNetLogger')
-                self.logger.setLevel(logging.DEBUG)
-                Logger.__instance = self
-            else:
-                raise SingletonError('This is a singleton class')
+        if Logger.__instance is None:
+            self.logger = logging.getLogger('QuantumNetLogger')
+            self.logger.handlers.clear()
+            self.logger.addHandler(logging.NullHandler())
+            self.logger.setLevel(logging.DEBUG)
+            Logger.__instance = self
+        else:
+            raise SingletonError('This is a singleton class')
 
     def get_instance():
         if Logger.__instance is None:
             Logger()
         return Logger.__instance
 
-    def activate(self):
-        Logger.DISABLED = False
+    def activate(self, level='INFO', console=False, file_log=True, filename=None):
+        self.logger.handlers.clear()
+
+        fmt = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+        log_level = getattr(logging, level.upper(), logging.INFO)
+
+        if file_log:
+            if filename is None:
+                timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+                filename = f'quantumnet_{timestamp}.log'
+            fh = logging.FileHandler(filename)
+            fh.setFormatter(fmt)
+            fh.setLevel(log_level)
+            self.logger.addHandler(fh)
+
+        if console:
+            sh = logging.StreamHandler()
+            sh.setFormatter(fmt)
+            sh.setLevel(log_level)
+            self.logger.addHandler(sh)
 
     def warn(self, message):
-        if not Logger.DISABLED:
-            self.logger.warning(message)
+        self.logger.warning(message)
 
     def error(self, message):
-        if not Logger.DISABLED:
-            self.logger.error(message)
+        self.logger.error(message)
 
     def log(self, message):
-        if not Logger.DISABLED:
-            self.logger.info(message)
+        self.logger.info(message)
 
     def debug(self, message):
-        if not Logger.DISABLED:
-            self.logger.debug(message)
+        self.logger.debug(message)
